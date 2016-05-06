8fa89eab5528c16148da2bf5f85fdb82a5b942ed
paavani <pavanisd9@gmail.com>
2016-05-06 11:43:44 +0530

    add model hook to route

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+var songs=[{
+
+	Title: 'Black Dog',
+	Band:  'Led Zeppelin',
+	Rating: 3
+}, {
+	Title: 'Yellow Ledbetter',
+	Band:  'Pearljam',
+	Rating: 4
+}, {
+	Title: 'The Pretender',
+	Band:  'Foo Fighters',
+	Rating: 2
+}];
 export default Ember.Route.extend({
+	model() {
+		return songs;
+	}		
 });
